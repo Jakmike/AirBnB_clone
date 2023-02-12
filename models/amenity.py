25acd6067ec50f534406b95ae91e5a17e6db1202
utibesky01 <utibesky01@gmail.com>
2023-02-12 09:40:55 -0800

    AirBnB project

---
+++ models/amenity.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This module defines the class `Amenity` """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Represents an amenity in the AirBnB
